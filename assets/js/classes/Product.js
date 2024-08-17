d05a1aad698c1e5a4639016b1445acb81b4ab983
Sebastián Segura Osorio <sseguraosorio@gmail.com>
2024-08-17 13:22:39 -0400

    relleno contenido de modal al hacer click sobre card de producto

--- assets/js/classes/Product.js
+++ assets/js/classes/Product.js
@@ -1,5 +1,5 @@
 export class Product {
-  constructor({id, title, price, description, category, image, rating}) {
+  constructor({ id, title, price, description, category, image, rating }) {
     this.id = id;
     this.title = title;
     this.price = price;
@@ -11,9 +11,9 @@ export class Product {
 
   createCardElement() {
     const card = document.createElement("article");
-    card.className = "col-12 col-sm-6 col-md-4 col-lg-3 ";
+    card.className = "col-12 col-sm-6 col-md-4 col-lg-3 product-card";
     card.innerHTML = `
-       <div class="card h-100">
+       <div class="card h-100 cursor-pointer">
             <img class="card-img-top h-100 object-fit-cover" src="${this.image}" alt="${this.title}" />
             <div class="card-body">
                 <h3 class="card-title">${this.title}</h3>
@@ -22,5 +22,16 @@ export class Product {
        </div>   
     `;
     return card;
+  }
+
+  createModalContent() {
+    return `
+        <img class="img-fluid mb-3" src="${this.image}" alt="${this.title}" />
+        <h4>${this.title}</h4>
+        <p>${this.description}</p>
+        <p><strong>Category:</strong> ${this.category}</p>
+        <p><strong>Price:</strong> ${this.price}</p>
+        <p><strong>Rating:</strong> ${this.rating.rate} (${this.rating.count})</p>
+    `;
   }
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,17 @@
-import { fetchProducts } from "./api.js";
+import { fetchProductById, fetchProducts } from "./api.js";
 import { Product } from "./classes/Product.js";
+
+const showProductModal = async (id) => {
+  const productData = await fetchProductById(id);
+  if (!productData) return;
+
+  const modalBody = document.querySelector(".modal-body");
+  const product = new Product(productData);
+  modalBody.innerHTML = product.createModalContent();
+
+  const myModal = new bootstrap.Modal(document.getElementById("productModal"));
+  myModal.show();
+};
 
 const showProducts = async () => {
   const products = await fetchProducts(); /* obtener productos de la API */
@@ -10,6 +22,10 @@ const showProducts = async () => {
   products.forEach((productItem) => {
     const product = new Product(productItem);
     const productCard = product.createCardElement();
+
+    productCard.addEventListener("click", () => {
+      showProductModal(product.id);
+    });
 
     rowContainer.appendChild(productCard);
   });
